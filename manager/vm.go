f3825102dbe5ba406e1090ecef494d501ac1350d
andreaba0 <andreabarchietto01@gmail.com>
2025-07-20 02:04:20 +0200

    init vm lifecycle

--- manager/vm.go
+++ manager/vm.go
@@ -13,7 +13,23 @@ const (
 )
 
 type VirtualMachine struct {
-	State    VirtualMachineState
-	Manifest *vmstorage.Manifest
-	PID      *int
+	Manifest          *Manifest
+	PID               *int
+	fileSystemStorage *vmstorage.FileSystemStorage
+}
+
+func (vm *VirtualMachine) Create() {
+
+}
+
+func (vm *VirtualMachine) Boot() {
+	var _ string = vm.fileSystemStorage.GetSocketPath(vm.Manifest.GuestName)
+}
+
+func (vm *VirtualMachine) Shutdown() {
+
+}
+
+func (vm *VirtualMachine) GetState() VirtualMachineState {
+	return VirtualMachineState(Unknow)
 }

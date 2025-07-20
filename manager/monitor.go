2cb583dba0f059d383af96ba844652f144a1c933
andreaba0 <andreabarchietto01@gmail.com>
2025-07-20 02:03:54 +0200

    add logger

--- manager/monitor.go
+++ manager/monitor.go
@@ -1,6 +1,11 @@
 package vmmanager
 
-import vmstorage "vmm/storage"
+import (
+	"encoding/json"
+	vmstorage "vmm/storage"
+
+	"go.uber.org/zap"
+)
 
 type VMConfig struct {
 	RootfsName    string
@@ -14,24 +19,30 @@ type VMConfig struct {
 type HypervisorMonitor struct {
 	VirtualMachines map[string]VirtualMachine
 	fs              vmstorage.FileSystemStorage
+	logger          *zap.Logger
 }
 
-func NewHypervisorMonitor(fs vmstorage.FileSystemStorage) *HypervisorMonitor {
+func NewHypervisorMonitor(fs vmstorage.FileSystemStorage, logger *zap.Logger) *HypervisorMonitor {
 	return &HypervisorMonitor{
-		fs: fs,
+		fs:     fs,
+		logger: logger,
 	}
 }
 
-func (hm *HypervisorMonitor) LoadVirtualMachines(runningInstances []RunningCHInstance, manifestList []vmstorage.Manifest) error {
+func (hm *HypervisorMonitor) LoadVirtualMachines(runningInstances []RunningCHInstance, manifestContentList []vmstorage.FileContent) error {
 	var i int
 	var err error
-	for i = 0; i < len(manifestList); i++ {
-		var manifest = manifestList[i]
+	for i = 0; i < len(manifestContentList); i++ {
+		var manifest *Manifest = &Manifest{}
+		err = json.Unmarshal(manifestContentList[i].Content, manifest)
+		if err != nil {
+			hm.logger.Error("Unable to parse file content. Maybe a corrupted file?", zap.String("path", manifestContentList[i].Path))
+			return err
+		}
 		var vmId = manifest.GuestName
 		hm.VirtualMachines[vmId] = VirtualMachine{
 			PID:      nil,
-			State:    Unknow,
-			Manifest: &manifest,
+			Manifest: manifest,
 		}
 	}
 	for i = 0; i < len(runningInstances); i++ {
@@ -44,6 +55,7 @@ func (hm *HypervisorMonitor) LoadVirtualMachines(runningInstances []RunningCHIns
 		var ok bool
 		_, ok = hm.VirtualMachines[vmId]
 		if !ok {
+			hm.logger.Error("There is an instance running without a manifest", zap.String("path", hm.fs.GetManifestPath(instance.UnixSocketPath)))
 			continue
 		}
 		if vmId != hm.VirtualMachines[vmId].Manifest.GuestName {
@@ -52,12 +64,7 @@ func (hm *HypervisorMonitor) LoadVirtualMachines(runningInstances []RunningCHIns
 		hm.VirtualMachines[vmId] = VirtualMachine{
 			PID:      &instance.PID,
 			Manifest: hm.VirtualMachines[vmId].Manifest,
-			State:    hm.VirtualMachines[vmId].State,
 		}
 	}
 	return nil
-}
-
-type HypervisorMonitorService interface {
-	LoadVirtualMachines(runningInstances []RunningCHInstance, manifestList []vmstorage.Manifest) error
 }
